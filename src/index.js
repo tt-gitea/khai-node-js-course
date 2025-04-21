a3c958a113e83ddc62f12911cdc88bf65105601e
tt-gitea <t.o.tuygunov@student.khai.edu>
2025-04-22 00:19:47 +0300

    create products module

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const userRouter = require('./user/user.router');
 const {logRequest} = require('./middleware');
 const bodyParser = require('body-parser');
 const {errorResponder} = require('./error.middleware');
+const productRouter = require('./product/product.router');
 
 const app = express();
 const PORT = 3000;
@@ -12,6 +13,8 @@ app.use(bodyParser.json());
 app.use(logRequest);
 app.use(productRoutes);
 app.use(userRouter)
+app.use(productRouter);
+
 app.use(errorResponder);
 
 app.listen(PORT, () => {
